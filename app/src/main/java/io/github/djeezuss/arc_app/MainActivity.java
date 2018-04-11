9838e29de6513e0da37ef5bf857203d68623add3
djeezuss <olivierleb25@gmail.com>
2018-04-11 15:10:31 -0400

    Added some labels, textboxes and a submit button

--- app/src/main/java/io/github/djeezuss/arc_app/MainActivity.java
+++ app/src/main/java/io/github/djeezuss/arc_app/MainActivity.java
@@ -1,13 +1,22 @@
 package io.github.djeezuss.arc_app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button bt_submit = findViewById(R.id.bt_submit);
+        bt_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }

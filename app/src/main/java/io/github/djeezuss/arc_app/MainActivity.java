bd7249ae11f93ca3befa1423951d71660f839e1f
Djeezuss <olivierleb25@gmail.com>
2018-04-16 23:04:44 -0400

    The IP address is shown

--- app/src/main/java/io/github/djeezuss/arc_app/MainActivity.java
+++ app/src/main/java/io/github/djeezuss/arc_app/MainActivity.java
@@ -1,15 +1,46 @@
 package io.github.djeezuss.arc_app;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
+    @Nullable
+    public static String getLocalIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                        return inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String ipAddr = getLocalIpAddress();
+        final TextView lb_IPAdrre = findViewById(R.id.lb_IPAddre);
+        lb_IPAdrre.setText(ipAddr);
 
         final Button bt_submit = findViewById(R.id.bt_submit);
         bt_submit.setOnClickListener(new View.OnClickListener() {
